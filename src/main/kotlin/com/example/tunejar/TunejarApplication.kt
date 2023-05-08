e0d9b00ce6eadaf1a087ea36ca91a8f19b606b02
JhoanaVicente <126072279+JhoanaVicente@users.noreply.github.com>
2023-05-08 14:51:24 +0200

    first commit

---
+++ src/main/kotlin/com/example/tunejar/TunejarApplication.kt
@@ -0,0 +1,11 @@
+package com.example.tunejar
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class TunejarApplication
+
+fun main(args: Array<String>) {
+    runApplication<TunejarApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/tunejar/TunejarApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.tunejar
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class TunejarApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
