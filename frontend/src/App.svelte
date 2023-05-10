ce5061e9007daafcf9807b2db0d2140903f909e7
mari19-83 <126073882+mari19-83@users.noreply.github.com>
2023-05-10 15:27:45 +0200

    arreglarcoverUrl

--- frontend/src/App.svelte
+++ frontend/src/App.svelte
@@ -9,7 +9,7 @@
     // {
     //     nombreCancion: "La Macarena",
     //     artista: "los del Rio",
-    //     coverImage: "",
+    //     coverUrl: "",
     //     generoMusical: "",
     //     releaseYear: ""
     // }
@@ -19,21 +19,21 @@
             genre:"",
             year:"",
             author: "los del Rio",
-            coverImage: "https://upload.wikimedia.org/wikipedia/commons/thumb/0/0b/%28Portrait_of_Billie_Holiday_and_Mister%2C_Downbeat%2C_New_York%2C_N.Y.%2C_ca._Feb._1947%29_%28LOC%29_%285020400274%29_%28cropped%29.jpg/800px-%28Portrait_of_Billie_Holiday_and_Mister%2C_Downbeat%2C_New_York%2C_N.Y.%2C_ca._Feb._1947%29_%28LOC%29_%285020400274%29_%28cropped%29.jpg"
+            coverUrl: "https://upload.wikimedia.org/wikipedia/commons/thumb/0/0b/%28Portrait_of_Billie_Holiday_and_Mister%2C_Downbeat%2C_New_York%2C_N.Y.%2C_ca._Feb._1947%29_%28LOC%29_%285020400274%29_%28cropped%29.jpg/800px-%28Portrait_of_Billie_Holiday_and_Mister%2C_Downbeat%2C_New_York%2C_N.Y.%2C_ca._Feb._1947%29_%28LOC%29_%285020400274%29_%28cropped%29.jpg"
         },
         {
             title: "Despacito",
             genre:"",
             year:"",
             author: "Luis Fonsi",
-            coverImage: "https://upload.wikimedia.org/wikipedia/commons/thumb/0/0b/%28Portrait_of_Billie_Holiday_and_Mister%2C_Downbeat%2C_New_York%2C_N.Y.%2C_ca._Feb._1947%29_%28LOC%29_%285020400274%29_%28cropped%29.jpg/800px-%28Portrait_of_Billie_Holiday_and_Mister%2C_Downbeat%2C_New_York%2C_N.Y.%2C_ca._Feb._1947%29_%28LOC%29_%285020400274%29_%28cropped%29.jpg"
+            coverUrl: "https://upload.wikimedia.org/wikipedia/commons/thumb/0/0b/%28Portrait_of_Billie_Holiday_and_Mister%2C_Downbeat%2C_New_York%2C_N.Y.%2C_ca._Feb._1947%29_%28LOC%29_%285020400274%29_%28cropped%29.jpg/800px-%28Portrait_of_Billie_Holiday_and_Mister%2C_Downbeat%2C_New_York%2C_N.Y.%2C_ca._Feb._1947%29_%28LOC%29_%285020400274%29_%28cropped%29.jpg"
         },
         {
             title: "Candy Shop",
             genre:"",
             year:"",
             author: "50 cent",
-            coverImage: "https://upload.wikimedia.org/wikipedia/commons/thumb/0/0b/%28Portrait_of_Billie_Holiday_and_Mister%2C_Downbeat%2C_New_York%2C_N.Y.%2C_ca._Feb._1947%29_%28LOC%29_%285020400274%29_%28cropped%29.jpg/800px-%28Portrait_of_Billie_Holiday_and_Mister%2C_Downbeat%2C_New_York%2C_N.Y.%2C_ca._Feb._1947%29_%28LOC%29_%285020400274%29_%28cropped%29.jpg"
+            coverUrl: "https://upload.wikimedia.org/wikipedia/commons/thumb/0/0b/%28Portrait_of_Billie_Holiday_and_Mister%2C_Downbeat%2C_New_York%2C_N.Y.%2C_ca._Feb._1947%29_%28LOC%29_%285020400274%29_%28cropped%29.jpg/800px-%28Portrait_of_Billie_Holiday_and_Mister%2C_Downbeat%2C_New_York%2C_N.Y.%2C_ca._Feb._1947%29_%28LOC%29_%285020400274%29_%28cropped%29.jpg"
         }]
 </script>
 
