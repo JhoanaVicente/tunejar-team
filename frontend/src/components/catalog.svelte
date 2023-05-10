b7ff21deaf3db16753109f052596297a0dc9e696
TatianaKedrova <127103022+TatianaKedrova@users.noreply.github.com>
2023-05-10 10:58:53 +0200

    [frontend] componente catalog y a tatiana se le ha caido agua en el ordenador

---
+++ frontend/src/components/catalog.svelte
@@ -0,0 +1,17 @@
+<script>
+
+    export let songs;
+</script>
+
+<div class="catalog">
+    <h1>Catalogo</h1>
+    <div>
+        {#each songs as song}
+          <Song/>
+
+
+        {/each}
+    </div>
+</div>
+
+
