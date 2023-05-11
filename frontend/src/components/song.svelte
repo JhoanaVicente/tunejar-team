b46d949d3f6c45dd08b6bbab28bdd4092b1ddbd9
mari19-83 <126073882+mari19-83@users.noreply.github.com>
2023-05-11 12:03:10 +0200

    tamaño de letra

--- frontend/src/components/song.svelte
+++ frontend/src/components/song.svelte
@@ -9,12 +9,15 @@
 
 <style>
     img {
-        width: 95px;
+        width: 110px;
         border-radius: 30%;
     }
 
     p {
         color: white;
+        font-size: 0.7em;
+        text-align: center;
+        line-height: 0.2em;
     }
     div {
         padding: 10px;
