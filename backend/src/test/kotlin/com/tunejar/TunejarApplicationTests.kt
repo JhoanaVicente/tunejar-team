d69ee1214c25a0e04cf870564b02ae1f22704dea
JhoanaVicente <126072279+JhoanaVicente@users.noreply.github.com>
2023-05-11 18:20:13 +0200

    change english language

--- backend/src/test/kotlin/com/tunejar/TunejarApplicationTests.kt
+++ backend/src/test/kotlin/com/tunejar/TunejarApplicationTests.kt
@@ -29,60 +29,57 @@ class TunejarApplicationTests(@Autowired val mockMvc: MockMvc) {
     @AfterEach
     fun tearDown(){
         repositorioCanciones.deleteAll()
-
     }
     @Test
     @Throws(Exception::class)
-    fun `devolver la lista de canciones`() {
-        addTestCanciones()
-        mockMvc.perform(get("/cancion"))
+    fun `return the existing songs`() {
+        addTestSongs()
+        mockMvc.perform(get("/Songs"))
             .andExpect(status().isOk)
             .andExpect(jsonPath("$[*]", hasSize<Int>(3)))
-            .andExpect(jsonPath("$[0].coverImage", equalTo("https://tse3.mm.bing.net/th?id=OIP.ruT15mxQwIgt8k_MgycTtQHaEK&pid=Api&P=0")))
-            .andExpect(jsonPath("$[0].artista", equalTo("Fito y Fitipaldis")))
-                .andExpect(jsonPath("$[0].nombreCancion", equalTo("Soldadito Marinero")))
-                    .andExpect(jsonPath("$[0].generoMusical", equalTo("Pop Rock")))
-                        .andExpect(jsonPath("$[0].releaseYear", equalTo(2003)))
-                        .andExpect(jsonPath("$[1].coverImage", equalTo("https://tse3.mm.bing.net/th?id=OIP.BRnGT23SKPg7BH9N4pqqNwHaHa&pid=Api&P=0")))
-                        .andExpect(jsonPath("$[1].artista", equalTo("Maluma")))
-                            .andExpect(jsonPath("$[1].nombreCancion", equalTo("Felices los 4")))
-                                .andExpect(jsonPath("$[1].generoMusical", equalTo("Salsa")))
-                                    .andExpect(jsonPath("$[1].releaseYear", equalTo(2018)) )
-                                        .andExpect(jsonPath("$[2].coverImage", equalTo("https://tse4.mm.bing.net/th?id=OIP.hoVIUhPoLh608K9KdvW_BgHaEk&pid=Api&P=0")))
-                                        .andExpect(jsonPath("$[2].artista", equalTo("Manolo García")))
-                                            .andExpect(jsonPath("$[2].nombreCancion", equalTo("Insurrección")))
-                                                .andExpect(jsonPath("$[2].generoMusical", equalTo("Jangle Pop")))
-                                                    .andExpect(jsonPath("$[2].releaseYear", equalTo(1986)))
-                                                    .andDo(print())
-
+            .andExpect(jsonPath("$[0].coverUrl", equalTo("https://tse3.mm.bing.net/th?id=OIP.ruT15mxQwIgt8k_MgycTtQHaEK&pid=Api&P=0")))
+            .andExpect(jsonPath("$[0].author", equalTo("Fito y Fitipaldis")))
+            .andExpect(jsonPath("$[0].title", equalTo("Soldadito Marinero")))
+            .andExpect(jsonPath("$[0].genre", equalTo("Pop Rock")))
+            .andExpect(jsonPath("$[0].releaseYear", equalTo(2003)))
+            .andExpect(jsonPath("$[1].coverUrl", equalTo("https://tse3.mm.bing.net/th?id=OIP.BRnGT23SKPg7BH9N4pqqNwHaHa&pid=Api&P=0")))
+            .andExpect(jsonPath("$[1].author", equalTo("Maluma")))
+            .andExpect(jsonPath("$[1].title", equalTo("Felices los 4")))
+            .andExpect(jsonPath("$[1].genre", equalTo("Salsa")))
+            .andExpect(jsonPath("$[1].releaseYear", equalTo(2018)) )
+            .andExpect(jsonPath("$[2].coverUrl", equalTo("https://tse4.mm.bing.net/th?id=OIP.hoVIUhPoLh608K9KdvW_BgHaEk&pid=Api&P=0")))
+            .andExpect(jsonPath("$[2].author", equalTo("Manolo García")))
+            .andExpect(jsonPath("$[2].title", equalTo("Insurrección")))
+            .andExpect(jsonPath("$[2].genre", equalTo("Jangle Pop")))
+            .andExpect(jsonPath("$[2].releaseYear", equalTo(1986)))
+            .andDo(print())
     }
-private fun addTestCanciones() {
-    val canciones: List <Songs> = listOf(
-        Songs(
-            "https://tse3.mm.bing.net/th?id=OIP.ruT15mxQwIgt8k_MgycTtQHaEK&pid=Api&P=0",
-            "Fito y Fitipaldis",
-            "Soldadito Marinero",
-            "Pop Rock",
-            2003
-        ),
-        Songs(
-            "https://tse3.mm.bing.net/th?id=OIP.BRnGT23SKPg7BH9N4pqqNwHaHa&pid=Api&P=0",
-            "Maluma",
-            "Felices los 4",
-            "Salsa",
-            2018
-        ),
-        Songs(
-            "https://tse4.mm.bing.net/th?id=OIP.hoVIUhPoLh608K9KdvW_BgHaEk&pid=Api&P=0",
-            "Manolo García",
-            "Insurrección",
-            "Jangle Pop",
-            1986
-        )
-    )
-      canciones.forEach(repositorioCanciones::save)
 
+    private fun addTestSongs() {
+        val canciones: List<Songs> = listOf(
+            Songs(
+                "https://tse3.mm.bing.net/th?id=OIP.ruT15mxQwIgt8k_MgycTtQHaEK&pid=Api&P=0",
+                "Fito y Fitipaldis",
+                "Soldadito Marinero",
+                "Pop Rock",
+                2003
+            ),
+            Songs(
+                "https://tse3.mm.bing.net/th?id=OIP.BRnGT23SKPg7BH9N4pqqNwHaHa&pid=Api&P=0",
+                "Maluma",
+                "Felices los 4",
+                "Salsa",
+                2018
+            ),
+            Songs(
+                "https://tse4.mm.bing.net/th?id=OIP.hoVIUhPoLh608K9KdvW_BgHaEk&pid=Api&P=0",
+                "Manolo García",
+                "Insurrección",
+                "Jangle Pop",
+                1986
+            ),
+            Songs.forEach(repositorioCanciones::save),
+    }
+}
 
-}
-}
 
