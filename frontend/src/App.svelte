f0ce7c37540533d99f3af56969773f67bdaaa2ac
JhoanaVicente <126072279+JhoanaVicente@users.noreply.github.com>
2023-05-12 14:15:30 +0200

    conectando frontend y backend

--- frontend/src/App.svelte
+++ frontend/src/App.svelte
@@ -3,30 +3,13 @@
     import Catalog from "./components/catalog.svelte"
     import {onMount} from "svelte";
 
+    let songs = []
 
-    let songs = [ // esto tendria que venir del backend
-        {
-            title: "felises los 4",
-            genre:"reggeton",
-            year:"",
-            author: "Maluma",
-            coverUrl: "https://th.bing.com/th/id/OIP.BRnGT23SKPg7BH9N4pqqNwHaHa?pid=ImgDet&rs=1"
-        },
-        {
-            title: "Insurrección",
-            genre:"",
-            year:"",
-            author: "manolo garcia",
-            coverUrl: "https://th.bing.com/th/id/R.2ea177a46023e4516925a49886051a0a?rik=IFOuBq5njNrKTQ&pid=ImgRaw&r=0"
-        },
-        {
-            title: "Soldadito marinero",
-            genre:"",
-            year:"",
-            author: "Fito & fitipaldis",
-            coverUrl: "https://i.pinimg.com/originals/33/13/5b/33135bdd7301f77a09e3ccfca9a7de08.jpg"
-
-        }]
+    onMount(() => {
+        fetch("http://localhost:9065/songs")
+            .then(respuesta => respuesta.json())
+            .then(datos => songs = datos)
+    })
 
 </script>
 
