ec137340e8c1c62f0092b7c82ed2b7c54c52d61e
JhoanaVicente <126072279+JhoanaVicente@users.noreply.github.com>
2023-05-15 10:25:28 +0200

    changes cover images

--- backend/src/main/kotlin/com/tunejar/repositorio/DataLoader.kt
+++ backend/src/main/kotlin/com/tunejar/repositorio/DataLoader.kt
@@ -9,7 +9,7 @@ class DataLoader (private val repositorio: RepositorioCanciones) {
     @PostConstruct
     fun load() {
         val canciones = listOf(
-            Songs("https://tse3.mm.bing.net/th?id=OIP.ruT15mxQwIgt8k_MgycTtQHaEK&pid=Api&P=0",
+            Songs("https://cdns-images.dzcdn.net/images/cover/04914ec5fd3d7c7332cf597b20085ef4/350x350.jpg",
                 "Fito y Fitipaldis",
                 "Soldadito Marinero",
                 "Pop Rock",
@@ -19,7 +19,7 @@ class DataLoader (private val repositorio: RepositorioCanciones) {
                 "Felices los 4",
                 "Salsa",
                 2018),
-            Songs("https://tse4.mm.bing.net/th?id=OIP.hoVIUhPoLh608K9KdvW_BgHaEk&pid=Api&P=0",
+            Songs("https://i.discogs.com/fYgMzw1OiJ8ld9SecrRgxol07e6vq1QDXKm7hKgQ1pg/rs:fit/g:sm/q:40/h:300/w:300/czM6Ly9kaXNjb2dz/LWRhdGFiYXNlLWlt/YWdlcy9SLTIwOTg3/MTczLTE2MzY5NzQ2/ODgtMTk5MS5qcGVn.jpeg",
                 "Manolo García",
                 "Insurrección",
                 "Jangle Pop",
