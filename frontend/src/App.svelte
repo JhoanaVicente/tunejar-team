f19c6896c1bfe26eed9def0ed294f27d681f29b2
halima <halimabenk@hotmail.com>
2023-05-15 10:51:15 +0200

    fondo pantalla

--- frontend/src/App.svelte
+++ frontend/src/App.svelte
@@ -22,11 +22,7 @@
     .container {
         width: 100%;
         height: 100vh;
-<<<<<<< HEAD
         background-image: url("https://c4.wallpaperflare.com/wallpaper/355/120/60/music-style-80s-style-illustration-hd-wallpaper-preview.jpg");
-=======
-        background-image: url("https://slack-imgs.com/?c=1&o1=ro&url=https%3A%2F%2Fi.pinimg.com%2F236x%2F24%2F3f%2Fd6%2F243fd6a377a1a3fa4791921ee71ef2c5.jpg");
->>>>>>> 609377539ec0812e0d8c44318cbb9d0533d23f36
         /*background-image: url(/images/fondo.png);*/
         background-size: cover;
         background-repeat: no-repeat;
