9fcd51e3d23d89861c6f33b3c3a68fb6511fe92a
halima <halimabenk@hotmail.com>
2023-05-15 10:40:22 +0200

    fondo pantalla

--- frontend/src/App.svelte
+++ frontend/src/App.svelte
@@ -22,7 +22,7 @@
     .container {
         width: 100%;
         height: 100vh;
-        background-image: url("https://e0.pxfuel.com/wallpapers/915/862/desktop-wallpaper-colorful-abstract-swirl-pattern-art.jpg");
+        background-image: url("https://c4.wallpaperflare.com/wallpaper/355/120/60/music-style-80s-style-illustration-hd-wallpaper-preview.jpg");
         /*background-image: url(/images/fondo.png);*/
         background-size: cover;
         background-repeat: no-repeat;
