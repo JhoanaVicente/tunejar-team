eaef283c1e1cd663e08639e1dca133f127a50ef9
jutelma <jutelma07051980@gmail.com>
2023-05-16 14:39:46 +0200

    añadido test restemplate

--- backend/src/test/kotlin/com/tunejar/TunejarApplicationTests.kt
+++ backend/src/test/kotlin/com/tunejar/TunejarApplicationTests.kt
@@ -1,5 +1,93 @@
 package com.tunejar
 
+import com.tunejar.repositorio.RepositorioCanciones
+import com.tunejar.repositorio.Songs
+import org.assertj.core.api.ClassBasedNavigableIterableAssert.assertThat
+import org.hamcrest.Matchers.equalTo
+import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.boot.test.web.client.TestRestTemplate
+import org.springframework.boot.test.web.client.getForEntity
+import org.springframework.http.HttpStatus
+import sun.nio.cs.Surrogate.`is`
+
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+
+class TunejarApplicationTests {
+    @Autowired
+    private lateinit var api: TestRestTemplate
+
+    @Autowired
+    private lateinit var repositorioCanciones: RepositorioCanciones
+
+    @Test
+    fun `Return a list of songs`() {
+        val songs = listOf(
+            Songs(
+                "https://cdns-images.dzcdn.net/images/cover/04914ec5fd3d7c7332cf597b20085ef4/350x350.jpg",
+                "Fito y Fitipaldis",
+                "Soldadito Marinero",
+                "Pop Rock",
+                2003
+            ),
+            Songs(
+                "https://tse3.mm.bing.net/th?id=OIP.BRnGT23SKPg7BH9N4pqqNwHaHa&pid=Api&P=0",
+                "Maluma",
+                "Felices los 4",
+                "Salsa",
+                2018
+            ),
+            Songs(
+                "https://i.discogs.com/fYgMzw1OiJ8ld9SecrRgxol07e6vq1QDXKm7hKgQ1pg/rs:fit/g:sm/q:40/h:300/w:300/czM6Ly9kaXNjb2dz/LWRhdGFiYXNlLWlt/YWdlcy9SLTIwOTg3/MTczLTE2MzY5NzQ2/ODgtMTk5MS5qcGVn.jpeg",
+                "Manolo García",
+                "Insurrección",
+                "Jangle Pop",
+                1986
+            )
+
+        ).let{repositorioCanciones.saveAll(it)}
+        val response = api.getForEntity("/api/songs", Array<Songs>::class.java)
+
+        assertThat(response.statusCode, `is`(HttpStatus.OK))
+        assertThat(response.body, equalTo(Songs.toTypedArray()))
+
+
+
+    }
+
+    private fun addTestSongs() {
+        val songs: List<Songs> = listOf(
+            Songs(
+                "https://cdns-images.dzcdn.net/images/cover/04914ec5fd3d7c7332cf597b20085ef4/350x350.jpg",
+                "Fito y Fitipaldis",
+                "Soldadito Marinero",
+                "Pop Rock",
+                2003
+            ),
+            Songs(
+                "https://tse3.mm.bing.net/th?id=OIP.BRnGT23SKPg7BH9N4pqqNwHaHa&pid=Api&P=0",
+                "Maluma",
+                "Salsa4",
+                "Salsa",
+                2018
+            ),
+            Songs(
+                "https://i.discogs.com/fYgMzw1OiJ8ld9SecrRgxol07e6vq1QDXKm7hKgQ1pg/rs:fit/g:sm/q:40/h:300/w:300/czM6Ly9kaXNjb2dz/LWRhdGFiYXNlLWlt/YWdlcy9SLTIwOTg3/MTczLTE2MzY5NzQ2/ODgtMTk5MS5qcGVn.jpeg",
+                "Manolo García",
+                "Insurrección",
+                "Jangle Pop",
+                1986
+            )
+
+        )
+        songs.forEach(RepositorioCanciones::save)
+    }
+}
+
+
+/*
 
 import com.tunejar.repositorio.Songs
 import com.tunejar.repositorio.RepositorioCanciones
@@ -68,7 +156,7 @@ class TunejarApplicationTests(@Autowired val mockMvc: MockMvc) {
             Songs(
                 "https://tse3.mm.bing.net/th?id=OIP.BRnGT23SKPg7BH9N4pqqNwHaHa&pid=Api&P=0",
                 "Maluma",
-                "Felices los 4",
+                "Salsa4",
                 "Salsa",
                 2018
             ),
@@ -86,4 +174,5 @@ class TunejarApplicationTests(@Autowired val mockMvc: MockMvc) {
 }
 
 
+*/
 
