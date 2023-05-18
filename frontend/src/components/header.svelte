c6d8d6aea293d80251f447db28a7c3120d1b9bcd
jutelma <jutelma07051980@gmail.com>
2023-05-18 12:37:06 +0200

    aceptando cambios

--- frontend/src/components/header.svelte
+++ frontend/src/components/header.svelte
@@ -5,14 +5,13 @@
 <style>
     header{
         display: flex;
-<<<<<<< HEAD
+
         align-items: center;
         flex-direction: column;
         padding-top: 0.2em;
 
-=======
         justify-content: flex-end;
->>>>>>> 4ab16014dd5d26636d6764049d3fd5f7aa4e0273
+
     }
 
     h2 {
