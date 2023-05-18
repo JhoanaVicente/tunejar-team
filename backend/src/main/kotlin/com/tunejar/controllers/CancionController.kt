aa5cbbe8e3ade997c232f1ef57107a86b372301a
jutelma <jutelma07051980@gmail.com>
2023-05-18 11:13:18 +0200

    Añadido test springboot

--- backend/src/main/kotlin/com/tunejar/controllers/CancionController.kt
+++ backend/src/main/kotlin/com/tunejar/controllers/CancionController.kt
@@ -3,53 +3,19 @@ package com.tunejar.controllers
 
 import com.tunejar.repositorio.Songs
 import com.tunejar.repositorio.RepositorioCanciones
-import org.springframework.http.HttpStatus
 import org.springframework.web.bind.annotation.CrossOrigin
-import org.springframework.web.bind.annotation.DeleteMapping
 import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.PathVariable
-import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.PutMapping
-import org.springframework.web.bind.annotation.RequestBody
-import org.springframework.web.bind.annotation.ResponseStatus
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
 @CrossOrigin
 class CancionController(private val repositorioCanciones: RepositorioCanciones) {
 
-    @GetMapping("/hello")
-    fun hola(): String = "Hola desde el backend"
 
     @GetMapping("/songs")
-    fun allCancion(): List<Songs?>? {
+    fun allCancion(): List<Songs> {
         return repositorioCanciones.findAll()
-    }
-
-    @GetMapping("/songs/{id}")
-    fun findCancion(@PathVariable id: Long): Songs? {
-        return repositorioCanciones.findById(id).orElseThrow { SongsNotFoundException() }
-    }
-
-    @PostMapping("/songs")
-    fun addCancion(@RequestBody cancion: Songs): Songs? {
-        return repositorioCanciones.save(cancion)
-    }
-
-    @PutMapping("/songs")
-    fun updateCancionById(@RequestBody cancion: Songs): Songs? {
-        cancion.id?.let { repositorioCanciones.findById(it).orElseThrow { SongsNotFoundException() } }
-        return repositorioCanciones.save(cancion)
-    }
-
-    @DeleteMapping("/songs/{id}")
-    fun deleteCancionById(@PathVariable id: Long): Songs? {
-        val cancion: Songs = repositorioCanciones.findById(id).orElseThrow { SongsNotFoundException() }
-        repositorioCanciones.deleteById(id)
-        return cancion
     }
 }
 
-@ResponseStatus(code = HttpStatus.NOT_FOUND, reason = "songs not found")
-class SongsNotFoundException: RuntimeException()
 
--- backend/src/test/kotlin/com/tunejar/TunejarApplicationTests.kt
+++ backend/src/test/kotlin/com/tunejar/TunejarApplicationTests.kt
@@ -1,20 +1,17 @@
 package com.tunejar
 
-import com.sun.org.apache.xerces.internal.util.PropertyState.`is`
 import com.tunejar.repositorio.RepositorioCanciones
 import com.tunejar.repositorio.Songs
-import org.hamcrest.MatcherAssert
-import org.hamcrest.Matchers
+import org.hamcrest.MatcherAssert.assertThat
 import org.hamcrest.Matchers.equalTo
-
-
+import org.hamcrest.Matchers.`is`
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.boot.test.web.client.TestRestTemplate
 import org.springframework.http.HttpStatus
-
 import org.springframework.test.context.ActiveProfiles
+
 
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
@@ -28,7 +25,7 @@ class TunejarApplicationTests {
 
     @Test
     fun `Return a list of songs`() {
-        listOf(
+       val songs = listOf(
             Songs(
                 "https://cdns-images.dzcdn.net/images/cover/04914ec5fd3d7c7332cf597b20085ef4/350x350.jpg",
                 "Fito y Fitipaldis",
@@ -50,137 +47,20 @@ class TunejarApplicationTests {
                 "Jangle Pop",
                 1986
             )
+       ).let {repositorioCanciones.saveAll(it)}
 
-        ).let{repositorioCanciones.saveAll(it)}
+        val response = api.getForEntity("/api/songs", Array<Songs>::class.java);
 
-        val response = api.getForEntity("/api/songs", Array<Songs>::class.java)
-
-/*
-        asserthat(response.statusCode, `is`(HttpStatus.OK))
-        asserthat(response.statusCode, equalTo(Songs.toTypeArray))
-*/
-
-
-
-
+        assertThat(response.statusCode, `is`(HttpStatus.OK))
+        assertThat(response.body, equalTo(songs.toTypedArray()))
     }
 
-    private fun addTestSongs() {
-        val songs: List<Songs> = listOf(
-            Songs(
-                "https://cdns-images.dzcdn.net/images/cover/04914ec5fd3d7c7332cf597b20085ef4/350x350.jpg",
-                "Fito y Fitipaldis",
-                "Soldadito Marinero",
-                "Pop Rock",
-                2003
-            ),
-            Songs(
-                "https://tse3.mm.bing.net/th?id=OIP.BRnGT23SKPg7BH9N4pqqNwHaHa&pid=Api&P=0",
-                "Maluma",
-                "Salsa4",
-                "Salsa",
-                2018
-            ),
-            Songs(
-                "https://i.discogs.com/fYgMzw1OiJ8ld9SecrRgxol07e6vq1QDXKm7hKgQ1pg/rs:fit/g:sm/q:40/h:300/w:300/czM6Ly9kaXNjb2dz/LWRhdGFiYXNlLWlt/YWdlcy9SLTIwOTg3/MTczLTE2MzY5NzQ2/ODgtMTk5MS5qcGVn.jpeg",
-                "Manolo García",
-                "Insurrección",
-                "Jangle Pop",
-                1986
-            )
 
-        )
-        songs.forEach(repositorioCanciones::save)
-    }
 }
 
 
-/*
-
-import com.tunejar.repositorio.Songs
-import com.tunejar.repositorio.RepositorioCanciones
-import org.hamcrest.Matchers.equalTo
-import org.hamcrest.Matchers.hasSize
-import org.junit.jupiter.api.AfterEach
-import org.junit.jupiter.api.Test
-import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
-import org.springframework.boot.test.context.SpringBootTest
-import org.springframework.test.web.servlet.MockMvc
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get
-import org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath
-import org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
-import org.springframework.test.web.servlet.result.MockMvcResultHandlers.print
 
 
 
-@SpringBootTest(
-    classes = arrayOf(TunejarApplication::class),
-    webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-@AutoConfigureMockMvc
-class TunejarApplicationTests(@Autowired val mockMvc: MockMvc) {
 
-    @Autowired
-    private lateinit var repositorioCanciones: RepositorioCanciones
-
-    @AfterEach
-    fun tearDown(){
-        repositorioCanciones.deleteAll()
-    }
-    @Test
-    @Throws(Exception::class)
-    fun `return the existing songs`() {
-        addTestSongs()
-        mockMvc.perform(get("/Songs"))
-            .andExpect(status().isOk)
-            .andExpect(jsonPath("$[*]", hasSize<Int>(3)))
-            .andExpect(jsonPath("$[0].coverUrl", equalTo("https://cdns-images.dzcdn.net/images/cover/04914ec5fd3d7c7332cf597b20085ef4/350x350.jpg")))
-            .andExpect(jsonPath("$[0].author", equalTo("Fito y Fitipaldis")))
-            .andExpect(jsonPath("$[0].title", equalTo("Soldadito Marinero")))
-            .andExpect(jsonPath("$[0].genre", equalTo("Pop Rock")))
-            .andExpect(jsonPath("$[0].releaseYear", equalTo(2003)))
-            .andExpect(jsonPath("$[1].coverUrl", equalTo("https://tse3.mm.bing.net/th?id=OIP.BRnGT23SKPg7BH9N4pqqNwHaHa&pid=Api&P=0")))
-            .andExpect(jsonPath("$[1].author", equalTo("Maluma")))
-            .andExpect(jsonPath("$[1].title", equalTo("Felices los 4")))
-            .andExpect(jsonPath("$[1].genre", equalTo("Salsa")))
-            .andExpect(jsonPath("$[1].releaseYear", equalTo(2018)) )
-            .andExpect(jsonPath("$[2].coverUrl", equalTo("https://i.discogs.com/fYgMzw1OiJ8ld9SecrRgxol07e6vq1QDXKm7hKgQ1pg/rs:fit/g:sm/q:40/h:300/w:300/czM6Ly9kaXNjb2dz/LWRhdGFiYXNlLWlt/YWdlcy9SLTIwOTg3/MTczLTE2MzY5NzQ2/ODgtMTk5MS5qcGVn.jpeg")))
-            .andExpect(jsonPath("$[2].author", equalTo("Manolo García")))
-            .andExpect(jsonPath("$[2].title", equalTo("Insurrección")))
-            .andExpect(jsonPath("$[2].genre", equalTo("Jangle Pop")))
-            .andExpect(jsonPath("$[2].releaseYear", equalTo(1986)))
-            .andDo(print())
-    }
-
-    private fun addTestSongs() {
-        val songs: List<Songs> = listOf(
-            Songs(
-                "https://cdns-images.dzcdn.net/images/cover/04914ec5fd3d7c7332cf597b20085ef4/350x350.jpg",
-                "Fito y Fitipaldis",
-                "Soldadito Marinero",
-                "Pop Rock",
-                2003
-            ),
-            Songs(
-                "https://tse3.mm.bing.net/th?id=OIP.BRnGT23SKPg7BH9N4pqqNwHaHa&pid=Api&P=0",
-                "Maluma",
-                "Salsa4",
-                "Salsa",
-                2018
-            ),
-            Songs(
-                "https://i.discogs.com/fYgMzw1OiJ8ld9SecrRgxol07e6vq1QDXKm7hKgQ1pg/rs:fit/g:sm/q:40/h:300/w:300/czM6Ly9kaXNjb2dz/LWRhdGFiYXNlLWlt/YWdlcy9SLTIwOTg3/MTczLTE2MzY5NzQ2/ODgtMTk5MS5qcGVn.jpeg",
-                "Manolo García",
-                "Insurrección",
-                "Jangle Pop",
-                1986
-            )
-
-        )
-            songs.forEach(repositorioCanciones::save)
-    }
-}
-
-
-*/
 
